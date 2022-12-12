6b53a70e6b4315d19316985775261b050162367b
HRT-268 <hiluekhabho@gmail.com>
2022-12-11 17:38:24 -0800

    0th commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Input a number:");
+	scanf("%d", &n);
+	if (n >= 0)
+	{
+		printf("%d is a positive number \n", n);
+	}
+	else
+		printf("%d is a nagative number \n", n);
+	return (0);
+}
+
